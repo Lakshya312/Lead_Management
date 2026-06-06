ffcc45459933c608425a2ec743e10979c27f5ef8
Lakshya Pal <pallakshya077@gmail.com>
2026-06-06 18:31:34 +0530

    Completed app registration

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lead_management',
 ]
 
 MIDDLEWARE = [
@@ -83,7 +84,7 @@ DATABASES = {
         'OPTIONS': {
     'driver': 'ODBC Driver 18 for SQL Server',
     'extra_params': 'TrustServerCertificate=yes;Encrypt=no',
-        }
+        },
     }
 }
 
